5e9bf2b51c051168bfd4b3ea5effe30ff42d2f09
Gmedina12 <gina.medina.1994@gmail.com>
2024-02-01 10:43:25 -0500

    updating experience

--- src/components/Experience/TechExperience.jsx
+++ src/components/Experience/TechExperience.jsx
@@ -10,10 +10,10 @@ export const TechExperience = () => {
                     <p>
                         <h4>Helpme App</h4>
                     </p>
-                    <p>Mobile Developer Sept 2023 - Oct 2023</p>
+                    <p>Mobile Developer Dic 2023 - Currently    </p>
                     <ul>
                         <li>Development of a mobile application for a Startup.</li>
-                        <li>Frontend: Flutter (Dart), Styled Componentes, HTML5 and Backend request on Dart</li>
+                        <li>Frontend: Flutter (Dart), Styled Componentes, HTML5 and Backend request on</li>
                         <li>Version Control: Git and Github - ASANA for tickets, issues, and planning</li>
                         <li>Test: Manual Blackbox testing</li>
                         <li>Documentation Drafting on Notion</li>
@@ -22,25 +22,18 @@ export const TechExperience = () => {
 
                 <div className={style.techUsed}>
                     <h3 align="left">Languages and Tools:</h3>
-                    <p align="left">
-                        <a href="https://flutter.dev/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/flutterio-icon.svg" alt="Flutter" height="50" /></a>
-                        <a href="https://dart.dev/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/dartlang-icon.svg" alt="Dart" height="50" /></a>
-                        <a href="https://www.android.com/intl/en_in/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/android-original-wordmark.svg" alt="Android" height="50" /></a>
-                        <a href="https://github.com/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/git-scm-icon.svg" alt="Git" height="50" /></a>
-                        <a href="https://app.asana.com/" target="_blank"><img style="margin: 10px" src="https://cdn.worldvectorlogo.com/logos/asana-1.svg" alt="Asana" height="50" /></a>
-                        <a href="https://flutterflow.io/" target="_blank"><img style="margin: 10px" src="https://asset.brandfetch.io/id209IGdxm/ide6ubEL24.svg?updated=1700754040029" alt="FlutterFlow" height="50" /></a>
-                        <a href="https://www.gnu.org/software/bash/" target="_blank"><img style="margin: 10px" src="https://profilinator.rishav.dev/skills-assets/gnu_bash-icon.svg" alt="Bash" height="50" /></a>
-                    </p>
-                    <br />
+                    <p align="left"> <a href="https://dart.dev/" target="_blank" rel="noreferrer"> <img src="https://profilinator.rishav.dev/skills-assets/dartlang-icon.svg" alt="Dart" width="40" height="40" /> </a> <a href="https://flutter.dev/" target="_blank" rel="noreferrer"> <img src="https://profilinator.rishav.dev/skills-assets/flutterio-icon.svg" alt="express" width="40" height="40" /> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" /> </a> <a href="https://www.android.com/intl/en_in/" target="_blank" rel="noreferrer"> <img src="https://profilinator.rishav.dev/skills-assets/android-original-wordmark.svg" alt="android" width="40" height="40" /> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" /> </a> <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src="https://profilinator.rishav.dev/skills-assets/typescript-original.svg" alt="typescript" width="40" height="40" /> </a> <a href="https://app.asana.com/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/asana-1.svg" alt="asana" width="40" height="40" /> </a> <a href="https://flutterflow.io/" target="_blank" rel="noreferrer"> <img src="https://asset.brandfetch.io/id209IGdxm/ide6ubEL24.svg?updated=1700754040029" alt="flutterflow" width="40" height="40" /> </a> <a href="https://www.gnu.org/software/bash/" target="_blank" rel="noreferrer"> <img src="https://profilinator.rishav.dev/skills-assets/gnu_bash-icon.svg" alt="bash" width="40" height="40" /> </a> <a href="https://developer.android.com/?hl=es-419" target="_blank" rel="noreferrer"> <img src="https://static-00.iconduck.com/assets.00/android-studio-icon-486x512-zp9um7zl.png" alt="androidstudio" width="40" height="40" /></a></p>
+                    <br/>
 
                     <div>
                         <a href="https://www.linkedin.com/company/helpme-app/">
-                            <button className={style.playButton}>Company profile</button>
+                            <button className={style.playButton}>Company Profile</button>
                         </a>
                     </div>
-
+                    
                 </div>
             </div>
+            
             <div className={style.container}>
                 <div className={style.expTech}>
                     <p>
@@ -61,14 +54,14 @@ export const TechExperience = () => {
                     <h3 align="left">Languages and Tools:</h3>
                     <p align="left"> <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" /> </a> <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40" /> </a> <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40" /> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" /> </a> <a href="https://graphql.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="graphql" width="40" height="40" /> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" /> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" /> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" /> </a> <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40" /> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40" /> </a> <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40" /></a> <a href="https://www.apollographql.com/"><img src="https://cdn.simpleicons.org/apollographql/311C87" height="40" alt="apollographql logo" />
                     </a> </p>
-                    <br />
+                    <br/>
 
                     <div>
                         <a href="https://www.youtube.com/watch?v=ZBi1--mW4GA">
                             <button className={style.playButton}>Play on YouTube ▶</button>
                         </a>
                     </div>
-
+                    
                 </div>
             </div>
 
